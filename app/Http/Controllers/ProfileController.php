36d3a7d87254c7076c9b4189719640b821ac8a4f
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-11-16 20:25:45 +0600

    index method created, set two veriable & create a associative array

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function index($id){
+        $name = "Donal Trump";
+        $age = "75";
+        $data = [
+            "id"=> $id,
+            "name"=> $name,
+            "age"=> $age
+        ];
+        return $data;
+    }
 }
